e2cf9c9c640d8b4fcfb1342de6a76b10cde68a97
dreugh-41 <dreugh.phillips@cnu.edu>
2025-04-02 19:57:02 -0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from sailors import views
+from sailors.debug_views import debug_auth
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('debug-auth/', debug_auth, name='debug_auth'),
     path('', include('sailors.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('coaches/', include('coaches.urls')),
